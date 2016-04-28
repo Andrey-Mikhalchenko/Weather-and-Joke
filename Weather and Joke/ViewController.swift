705ef845750155e11b9eb39761652ae6cbf816be
Андрей Михальченко <andrejmihalcenko@msk-wifi-15fap8-a_mikhalchenko-a.mikhalchenko.mail.msk>
2016-04-28 22:12:02 +0300

    Initial Commit

---
+++ Weather and Joke/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather and Joke
+//
+//  Created by Андрей Михальченко on 28.04.16.
+//  Copyright © 2016 Mikhalchenko Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Weather and JokeTests/Weather_and_JokeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_and_JokeTests.swift
+//  Weather and JokeTests
+//
+//  Created by Андрей Михальченко on 28.04.16.
+//  Copyright © 2016 Mikhalchenko Corp. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_and_Joke
+
+class Weather_and_JokeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
